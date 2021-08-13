341a85e093f95d853e2e197d645347c4111bdee2
Fastion-1024 <dsprice1989@outlook.com>
2021-08-13 17:00:49 +0100

    Added Functionality to Display - Created function to format current input from state. - Created function to format formula from state. - Added basic JSX.

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -1,5 +1,26 @@
+import { useGlobalContext } from '../../hooks/context';
+
 const Display = () => {
-    return <div></div>;
+    const { input1, operator, input2, answer, isNegative } = useGlobalContext();
+
+    const getInput = () => {
+        if (answer) return answer;
+        if (!input1) return '0';
+        if (operator && input2) return input2;
+        return input1;
+    };
+
+    const getFormula = () => {
+        if (answer) return `${input1} ${operator} ${input2} =`;
+        if (input1 && operator) return `${input1} ${operator}`;
+    };
+
+    return (
+        <div className='display'>
+            <h3>{getFormula()}</h3>
+            <h2 id='display'>{getInput()}</h2>
+        </div>
+    );
 };
 
 export default Display;
