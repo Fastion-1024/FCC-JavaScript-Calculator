9c1fc41da62314cc15aef80df55f0b60cfe771e4
Fastion-1024 <dsprice1989@outlook.com>
2021-08-13 16:59:34 +0100

    Added Basic Functionality - Added new actions. - Added actions to reducer. - Added functionality for each action. - Updated initial state. - Created function to handle button click. - Created functions to handle each button type.

--- src/hooks/context.js
+++ src/hooks/context.js
@@ -1,10 +1,116 @@
 import React, { useContext, useReducer, useEffect } from 'react';
+import reducer from './reducer';
+import actions from './actions';
+import types from '../lib/types';
+
 const AppContext = React.createContext();
-const initialState = {};
+const initialState = {
+    input1: '',
+    operator: '',
+    input2: '',
+    lastType: '',
+    answer: '',
+    isNegative: false,
+};
 
 const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>;
+
+    const handleClick = (btn) => {
+        switch (btn.type) {
+            case types.DIGIT:
+                handleDigit(btn.value);
+                break;
+
+            case types.BINARY_OPERATOR:
+                handleOperator(btn.value);
+                break;
+
+            case types.DECIMAL:
+                handleDecimal();
+                break;
+
+            case types.EQUALS:
+                handleEquals();
+                break;
+
+            case types.FUNCTION:
+                handleFunction(btn.id);
+                break;
+        }
+    };
+
+    const handleDigit = (value) => {
+        if (state.operator) {
+            dispatch({ type: actions.ADD_DIGIT_TO_INPUT2, payload: value });
+        } else {
+            dispatch({ type: actions.ADD_DIGIT_TO_INPUT1, payload: value });
+        }
+
+        dispatch({ type: actions.UPDATE_LAST_TYPE, payload: types.DIGIT });
+    };
+
+    const handleOperator = (value) => {
+        if (!state.input1) {
+            dispatch({ type: actions.ADD_DIGIT_TO_INPUT1, payload: '0' });
+            dispatch({ type: actions.UPDATE_OPERATOR, payload: value });
+        } else if (state.input1 && state.input2) {
+            dispatch({ type: actions.CALCULATE_ANSWER });
+            dispatch({ type: actions.APPLY_OPERATION_TO_ANSWER, payload: value });
+        } else {
+            dispatch({ type: actions.UPDATE_OPERATOR, payload: value });
+        }
+
+        dispatch({ type: actions.UPDATE_LAST_TYPE, payload: types.BINARY_OPERATOR });
+    };
+
+    const handleDecimal = () => {
+        if (state.operator) {
+            dispatch({ type: actions.ADD_DECIMAL_TO_INPUT2 });
+        } else {
+            dispatch({ type: actions.ADD_DECIMAL_TO_INPUT1 });
+        }
+
+        dispatch({ type: actions.UPDATE_LAST_TYPE, payload: types.DECIMAL });
+    };
+
+    const handleEquals = () => {
+        if (state.answer) {
+            dispatch({ type: actions.CALCULATE_LAST_OPERATION_ON_ANSWER });
+        } else {
+            dispatch({ type: actions.CALCULATE_ANSWER });
+        }
+        dispatch({ type: actions.UPDATE_LAST_TYPE, payload: types.EQUALS });
+    };
+
+    const handleFunction = (id) => {
+        switch (id) {
+            case 'clear':
+                dispatch({ type: actions.CLEAR_CALCULATOR });
+                break;
+
+            case 'clearEntry':
+                if (state.operator) {
+                    dispatch({ type: actions.CLEAR_INPUT2 });
+                } else {
+                    dispatch({ type: actions.CLEAR_INPUT1 });
+                }
+                break;
+
+            case 'backspace':
+                if (state.operator) {
+                    dispatch({ type: actions.REMOVE_CHAR_FROM_INPUT2 });
+                } else {
+                    dispatch({ type: actions.REMOVE_CHAR_FROM_INPUT1 });
+                }
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    return <AppContext.Provider value={{ ...state, handleClick }}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => {
--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -1,6 +1,102 @@
+import math from 'mathjs/lib/browser/math';
+import actions from './actions';
+
 const reducer = (state, action) => {
     console.log(action.type, '-', action.payload);
+
     switch (action.type) {
+        case actions.ADD_DIGIT_TO_INPUT1:
+            // TODO: 0. is not possible overriden
+            return {
+                ...state,
+                input1:
+                    state.input1.charAt(0) === '0' ? action.payload : state.input1 + action.payload,
+            };
+
+        case actions.ADD_DIGIT_TO_INPUT2:
+            return {
+                ...state,
+                input2:
+                    state.input2.charAt(0) === '0' ? action.payload : state.input2 + action.payload,
+            };
+
+        case actions.UPDATE_OPERATOR:
+            return { ...state, operator: action.payload };
+
+        case actions.ADD_DECIMAL_TO_INPUT1:
+            if (state.input1.split('').includes('.')) return { ...state };
+            return { ...state, input1: !state.input1 ? '0.' : state.input1 + '.' };
+
+        case actions.ADD_DECIMAL_TO_INPUT2:
+            if (state.input2.split('').includes('.')) return { ...state };
+            return { ...state, input2: !state.input2 ? '0.' : state.input2 + '.' };
+
+        case actions.UPDATE_LAST_TYPE:
+            return { ...state, lastType: action.payload };
+
+        case actions.UPDATE_NEGATIVE_FLAG:
+            return { ...state, isNegative: action.payload };
+
+        case actions.CALCULATE_ANSWER:
+            if (state.input1 && !state.operator && !state.input2) {
+                return { ...state, answer: state.input1 };
+            }
+
+            if (state.input1 && state.operator && !state.input2) {
+                const result = math.string(
+                    math.evaluate(state.input1 + state.operator + state.input1)
+                );
+                return { ...state, input2: state.input1, answer: result };
+            }
+
+            if (state.input1 && state.operator && state.input2) {
+                const result = math.string(
+                    math.evaluate(state.input1 + state.operator + state.input2)
+                );
+                return { ...state, answer: result };
+            }
+
+            return { ...state };
+
+        case actions.APPLY_OPERATION_TO_ANSWER:
+            return {
+                ...state,
+                input1: state.answer,
+                operator: action.payload,
+                input2: '',
+                answer: '',
+                isNegative: false,
+            };
+
+        case actions.CALCULATE_LAST_OPERATION_ON_ANSWER:
+            const result = math.string(math.evaluate(state.answer + state.operator + state.input2));
+            return { ...state, input1: state.answer, answer: result };
+
+        case actions.CLEAR_INPUT1:
+            return { ...state, input1: '0' };
+
+        case actions.CLEAR_INPUT2:
+            return { ...state, input2: '0' };
+
+        case actions.CLEAR_CALCULATOR:
+            return {
+                ...state,
+                input1: '',
+                operator: '',
+                input2: '',
+                lastType: '',
+                answer: '',
+                isNegative: false,
+            };
+
+        case actions.REMOVE_CHAR_FROM_INPUT1:
+            const newInput1 = state.input1.slice(0, state.input1.length - 1);
+            return { ...state, input1: newInput1 ? newInput1 : '0' };
+
+        case actions.REMOVE_CHAR_FROM_INPUT2:
+            const newInput2 = state.input2.slice(0, state.input2.length - 1);
+            return { ...state, input2: newInput2 ? newInput2 : '0' };
+
         default:
             throw new Error('No matching action found!');
     }
