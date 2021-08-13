7933465e3e330f7c306cd90392297b5ce2d51530
Fastion-1024 <dsprice1989@outlook.com>
2021-08-13 17:02:01 +0100

    Updated Keypad Component - Keypad now maps each object from buttons.js to a button element. - Each button's onClick event will fire context.js handleClick function.

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,5 +1,20 @@
+import { useGlobalContext } from '../../hooks/context';
+import buttons from '../../lib/buttons';
+
 const Keypad = () => {
-    return <div></div>;
+    const { handleClick } = useGlobalContext();
+
+    return (
+        <div className='keypad'>
+            {buttons.map((btn) => {
+                return (
+                    <button key={btn.id} id={btn.id} onClick={() => handleClick(btn)}>
+                        {btn.value}
+                    </button>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Keypad;
