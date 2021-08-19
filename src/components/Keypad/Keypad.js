8735fd7a6c7bbb98e6af22b67cc17705bc08472a
Fastion-1024 <dsprice1989@outlook.com>
2021-08-19 14:09:52 +0100

    Inital Layout Styling for Keypad

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,25 +1,30 @@
 import { useGlobalContext } from '../../hooks/context';
 import buttons, { memoryButtons } from '../../lib/buttons';
+import './Keypad.css';
 
 const Keypad = () => {
     const { handleClick } = useGlobalContext();
 
     return (
         <div className='keypad'>
-            {buttons.map((btn) => {
-                return (
-                    <button key={btn.id} id={btn.id} onClick={() => handleClick(btn)}>
-                        {btn.value}
-                    </button>
-                );
-            })}
-            {memoryButtons.map((btn) => {
-                return (
-                    <button key={btn.id} id={btn.id} onClick={() => handleClick(btn)}>
-                        {btn.value}
-                    </button>
-                );
-            })}
+            <div className='btn-container-memory'>
+                {memoryButtons.map((btn) => {
+                    return (
+                        <button key={btn.id} id={btn.id} onClick={() => handleClick(btn)}>
+                            {btn.value}
+                        </button>
+                    );
+                })}
+            </div>
+            <div className='btn-container-default'>
+                {buttons.map((btn) => {
+                    return (
+                        <button key={btn.id} id={btn.id} onClick={() => handleClick(btn)}>
+                            {btn.value}
+                        </button>
+                    );
+                })}
+            </div>
         </div>
     );
 };
