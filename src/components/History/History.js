04a1e762bfd60f7a3fdbf61c32569c3b37d4ee21
Fastion-1024 <dsprice1989@outlook.com>
2021-08-19 14:11:10 +0100

    Initial Layout Styling for History

--- src/components/History/History.js
+++ src/components/History/History.js
@@ -1,20 +1,27 @@
 import { useGlobalContext } from '../../hooks/context';
+import './History.css';
 
 const History = () => {
     const { history, clearHistory, removeItemFromHistory, restoreItemFromHistory } =
         useGlobalContext();
 
     return (
-        <section>
-            {history.map((item, index) => {
-                const { input1, operator, input2, answer } = item;
-                return (
-                    <div key={index} onClick={() => restoreItemFromHistory(item)}>
-                        <p>{`${input1} ${operator} ${input2} =`}</p>
-                        <p>{answer}</p>
-                    </div>
-                );
-            })}
+        <section className='history-container'>
+            <div className='history-item-container'>
+                {history.map((item, index) => {
+                    const { input1, operator, input2, answer } = item;
+                    return (
+                        <div
+                            key={index}
+                            className='history-item'
+                            onClick={() => restoreItemFromHistory(item)}
+                        >
+                            <p>{`${input1} ${operator} ${input2} =`}</p>
+                            <p>{answer}</p>
+                        </div>
+                    );
+                })}
+            </div>
             <button onClick={clearHistory}>
                 <span>Delete History</span>
             </button>
