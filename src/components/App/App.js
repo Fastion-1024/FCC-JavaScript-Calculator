2f39b281b627f2ce5a6b52640521cbceab61465d
Fastion-1024 <dsprice1989@outlook.com>
2021-08-19 14:11:51 +0100

    Removed Elements - Removed History. - Removed Memory.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,10 @@
 import Calculator from '../Calculator/Calculator';
-import History from '../History/History';
-import Memory from '../Memory/Memory';
 import './App.css';
 
 function App() {
     return (
         <main>
             <Calculator />
-            <History />
-            <Memory />
         </main>
     );
 }
