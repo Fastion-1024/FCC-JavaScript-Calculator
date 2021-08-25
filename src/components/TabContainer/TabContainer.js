aaf9ff2fdb4a78b9ca7164f25ce4bd725742cd29
Fastion-1024 <dsprice1989@outlook.com>
2021-08-25 15:37:39 +0100

    Added Active Class to Headers - An Active class will be added to the last clicked header.

--- src/components/TabContainer/TabContainer.js
+++ src/components/TabContainer/TabContainer.js
@@ -1,16 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './TabContainer.css';
 
 const TabContainer = ({ children, headers }) => {
     const [currentChild, setCurrentChild] = useState(null);
+    const buttonRefs = useRef([]);
 
     const handleClick = (id) => {
         setCurrentChild(children[id]);
+        switchActiveElement(id);
+    };
+
+    const switchActiveElement = (id) => {
+        buttonRefs.current.forEach((element, index) => {
+            id !== index && element.classList.remove('active');
+            id === index && element.classList.add('active');
+        });
     };
 
     useEffect(() => {
         if (headers.length > 0 && children.length > 0) {
             setCurrentChild(children[0]);
+            switchActiveElement(0);
         }
     }, []);
 
@@ -19,7 +29,11 @@ const TabContainer = ({ children, headers }) => {
             <div className='tab-headers'>
                 {headers.map((btn, index) => {
                     return (
-                        <button key={index} onClick={() => handleClick(index)}>
+                        <button
+                            key={index}
+                            ref={(element) => (buttonRefs.current[index] = element)}
+                            onClick={() => handleClick(index)}
+                        >
                             {btn}
                         </button>
                     );
